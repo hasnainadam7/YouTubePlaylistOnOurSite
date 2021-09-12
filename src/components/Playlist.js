6d388db07e8cb27cf32ebfef02087042d44775d2
hasnainadam7 <79312974+hasnainadam7@users.noreply.github.com>
2021-09-12 04:00:53 -0700

    Work Done

---
+++ src/components/Playlist.js
@@ -0,0 +1,60 @@
+import React from "react"
+import { useEffect, useState } from 'react'
+import "./style.css"
+
+
+const MenuCard = () => {
+    const [data, setData] = useState()
+    const playlist = "PLzGHKb8i9vTwhFKH2lTKre_UHofw3hGiD"
+    const Fetchdata = async () => {
+        const setHeader = {
+            headers: {
+                Accept: "application/json"
+            }
+        }
+        try {
+            const maximumvideos = 6
+            const { REACT_APP_API } = process.env
+            const api = `https://www.googleapis.com/youtube/v3/playlistItems?part=contentDetails,snippet&playlistId=${playlist}&maxResults=${maximumvideos}&key=${REACT_APP_API}`
+            const respones = await fetch(api, setHeader)
+            const jsonData = await respones.json();
+            console.log(jsonData);
+            setData(jsonData)
+
+        }
+        catch (err) {
+            console.log("The Error is " + err)
+        }
+
+    }
+    useEffect(() => {
+        return Fetchdata();
+    }, [])
+
+    if (data) {
+        return <>
+            {data.items.map((item, id) => {
+                const { title } = item.snippet
+                const videoId = item.contentDetails.videoId
+                const playlists = "https://www.youtube.com/embed/" + videoId + "?list=" + playlist
+                return <>
+                    <div className="card-container" key={id}>
+                        <div className="card">
+                            <div className="card-body">
+                                <h3 className="card-title">{title}</h3>
+                                <iframe width="1000" className="card-media" height="500" src={playlists}
+                                    title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+                            </div>
+                        </div>
+                    </div>
+                </>
+            })
+            }
+        </>
+    }
+    else if (!data) {
+        return <> Please Wait Loading</>
+    }
+}
+
+export default MenuCard;
